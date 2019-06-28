ac1a8ae7d883dedd59293d92f41f8a6e82683039
yaojiaqi <jiaqi.yao@brilliantaero.com>
2019-06-28 15:18:49 +0800

    update1.0

--- README.md
+++ README.md
@@ -1,6 +1,21 @@
 # showEachTestRuntime
-### usage:
-  
-​	   **import this module in manage.py**
-​	   **then run your django test with addition command: "\--time"**
+# usage:
+
+- You can put the script anywhere in your django project
+
+- Then you need to use the following line of code in settings.py
+
+  ```
+  TEST_RUNNER = 'path.DiscoverSlowestTestsRunner' # the path is where you put the script
+  ```
+
+- When you run the djagno test you can have addition commands:
+
+  `--time=300`,The console will output the execution time of each test instead of the test run info. while the time is greater than 300 ms, it will turn red.
+
+  `--report=300` When all test runs are completed, the console will output a test report with a run time greater than 300 ms.
+
+  `--limit=15` The final test report will only show the slowest 15 data
+
+
 
--- main.py
+++ main.py
@@ -1,53 +1,318 @@
+import glob
+import json
+import time
 import sys
+import os
+import os.path
+import logging
+from unittest import TestSuite, TestLoader
+from unittest.runner import TextTestRunner, registerResult
+from unittest.suite import _isnotsuite
+from django.test.runner import DiscoverRunner
+from django.conf import settings
+
+try:  # pragma: no cover
+    import freezegun
 
 
-vaild_time = 0.7
-# add support for --time option to measure the time needed to execute each Test Class
-if 'test' in sys.argv and '--time' in sys.argv:
-    sys.argv.remove('--time')
+    def _time():
+        return freezegun.api.real_time()
+except ImportError:  # pragma: no cover
+    def _time():
+        return time.time()
 
-    # disable logging during tests
-    import logging
-    logging.disable(logging.CRITICAL)
+time_only = False
+report = False
+# if time > valid(ms) the result will turn red
+valid_time = 300
+# the final report will only count results greater than slow_test_threshold_ms(ms)
+slow_test_threshold_ms = 0
+# the final report will display the longest slow_test_threshold_ms data
+num_slow_tests = None
 
-    import time
-    from django.test import TestCase
+if 'test' in sys.argv:
+    try:
+        for i in sys.argv[1:]:
+            if i.startswith("--time"):
+                sys.argv.remove(i)
+                try:
+                    param = i.split("=")[1]
+                    valid_time = int(param) / 1000 if int(param) > 0 else valid_time
+                except IndexError:
+                    pass
+                finally:
+                    # disable logging during tests
+                    time_only = True
+                    logging.disable(logging.CRITICAL)
 
-    @classmethod
-    def setUpClass(cls):
-        print("\n\033[95m%s.%s started\033[0m" % (cls.__module__, cls.__name__))
-        cls.class_start_time = time.time()
+            if i.startswith("--report"):
+                sys.argv.remove(i)
+                try:
+                    param = i.split("=")[1]
+                    slow_test_threshold_ms = int(param) if int(param) > 0 else slow_test_threshold_ms
+                except IndexError:
+                    pass
+                finally:
+                    report = True
 
-    @classmethod
-    def tearDownClass(cls):
-        runtime = time.time() - cls.class_start_time
-        if runtime < vaild_time:
-            runtime = f"\033[32m{runtime}\033[0m"
+            if i.startswith("--limit"):
+                sys.argv.remove(i)
+                try:
+                    param = i.split("=")[1]
+                    num_slow_tests = int(param) if int(param) > 0 else num_slow_tests
+                except IndexError:
+                    pass
+
+    except Exception:
+        print("Error invalid params")
+
+
+class TimingTextTestRunner(TextTestRunner):
+    def run(self, test):
+        "Run the given test case or test suite."
+        result = self._makeResult()
+        registerResult(result)
+        result.failfast = self.failfast
+        result.buffer = self.buffer
+        startTime = time.time()
+        startTestRun = getattr(result, 'startTestRun', None)
+        if startTestRun is not None:
+            startTestRun()
+        try:
+            test(result)
+        finally:
+            stopTestRun = getattr(result, 'stopTestRun', None)
+            if stopTestRun is not None:
+                stopTestRun()
+        stopTime = time.time()
+        timeTaken = stopTime - startTime
+        result.printErrors()
+        if hasattr(result, 'separator2'):
+            self.stream.writeln(result.separator2)
+        run = result.testsRun
+        self.stream.writeln("Ran %d test%s in %.3fs" %
+                            (run, run != 1 and "s" or "", timeTaken))
+        self.stream.writeln()
+
+        expectedFails = unexpectedSuccesses = skipped = 0
+        try:
+            results = map(len, (result.expectedFailures,
+                                result.unexpectedSuccesses,
+                                result.skipped))
+        except AttributeError:
+            pass
         else:
-            runtime = f"\033[31m{runtime}\033[0m"
+            expectedFails, unexpectedSuccesses, skipped = results
 
-        print("\n\033[94m%s.%s finished in %s seconds\033[0m" % (cls.__module__, cls.__name__,
-                                                                 runtime))
+        infos = []
+        if not result.wasSuccessful():
+            self.stream.write("FAILED")
+            failed, errored = map(len, (result.failures, result.errors))
+            if failed:
+                infos.append("failures=%d" % failed)
+            if errored:
+                infos.append("errors=%d" % errored)
+        else:
+            self.stream.write("OK")
+        if skipped:
+            infos.append("skipped=%d" % skipped)
+        if expectedFails:
+            infos.append("expected failures=%d" % expectedFails)
+        if unexpectedSuccesses:
+            infos.append("unexpected successes=%d" % unexpectedSuccesses)
+        if infos:
+            self.stream.writeln(" (%s)" % (", ".join(infos),))
+        else:
+            self.stream.write("\n")
+        # save timeTaken for later use in report
+        result.timeTaken = timeTaken
+        return result
 
-    TestCase.setUpClass = setUpClass
-    TestCase.tearDownClass = tearDownClass
 
-    # add support for --detailed option to measure the time needed to execute each test
-    if '--detailed' in sys.argv:
-        sys.argv.remove('--detailed')
+class TimingSuite(TestSuite):
+    """
+    TestSuite wrapper that times each test.
+    """
 
-        def setUp(self):
-            self.method_start_time = time.time()
+    def save_test_time(self, test_name, duration):
+        file_prefix = getattr(
+            settings, 'TESTS_REPORT_TMP_FILES_PREFIX', '_tests_report_'
+        )
+        file_name = '{}{}.txt'.format(file_prefix, os.getpid())
+        with open(file_name, "a+") as f:
+            f.write("{name},{duration:.6f}\n".format(
+                name=test_name, duration=duration
+            ))
 
-        def tearDown(self):
-            execution_time = time.time() - self.method_start_time
-            if execution_time < 1.5:
-                color = "\033[92m"
-            elif execution_time < 3.5:
-                color = "\033[93m"
+    def run(self, result, debug=False):
+        topLevel = False
+        if getattr(result, '_testRunEntered', False) is False:
+            result._testRunEntered = topLevel = True
+
+        for test in self:
+            if result.shouldStop:
+                break
+
+            start_time = _time()
+
+            if _isnotsuite(test):
+                self._tearDownPreviousClass(test, result)
+                self._handleModuleFixture(test, result)
+                self._handleClassSetUp(test, result)
+                result._previousTestClass = test.__class__
+
+                if (getattr(test.__class__, '_classSetupFailed', False) or
+                        getattr(result, '_moduleSetUpFailed', False)):
+                    continue
+
+            if not debug:
+                test(result)
             else:
-                color = "\033[91m"
-            print("\n%s%s executed in %.3f seconds\033[0m" % (color, self._testMethodName, execution_time))
+                test.debug()
+            self.save_test_time(str(test), _time() - start_time)
 
-        TestCase.setUp = setUp
-        TestCase.tearDown = tearDown
+            if time_only:
+                timing = _time() - start_time
+                if timing > valid_time:
+                    print("\n\033[31m{t:.4f}s\033[0m \033[94m{f}\033[0m".format(f=str(test), t=float(timing)))
+                else:
+                    print("\n\033[32m{t:.4f}s\033[0m \033[94m{f}\033[0m".format(f=str(test), t=float(timing)))
+
+        if topLevel:
+            self._tearDownPreviousClass(None, result)
+            self._handleModuleTearDown(result)
+            result._testRunEntered = False
+
+        return result
+
+
+class TimingLoader(TestLoader):
+    suiteClass = TimingSuite
+
+
+class DiscoverSlowestTestsRunner(DiscoverRunner):
+    """
+    Runner that extends Django's DiscoverRunner to time the tests.
+    """
+    test_suite = TimingSuite
+    test_loader = TimingLoader()
+    test_runner = TimingTextTestRunner
+
+    def __init__(self, report_path=None, generate_report=False, **kwargs):
+        super(DiscoverSlowestTestsRunner, self).__init__(**kwargs)
+        self.report_path = report_path[0] if report_path else None
+        self.should_generate_report = generate_report
+
+    @classmethod
+    def add_arguments(cls, parser):
+        DiscoverRunner.add_arguments(parser)
+        parser.add_argument(
+            '--slowreport',
+            action='store_true',
+            dest='generate_report',
+            help='Generate a report of slowest tests',
+        )
+        parser.add_argument(
+            '--slowreportpath',
+            nargs=1,
+            dest='report_path',
+            help='Save report to given file'
+        )
+
+    def generate_report(self, test_results, result):
+        test_result_count = len(test_results)
+        SLOW_TEST_THRESHOLD_MS = slow_test_threshold_ms
+
+        if self.report_path:
+            data = {
+                'threshold': SLOW_TEST_THRESHOLD_MS,
+                'slower_tests': [
+                    {"name": func_name, "execution_time": float(timing)}
+                    for func_name, timing in test_results
+                ],
+                'nb_tests': result.testsRun,
+                'nb_failed': len(result.errors + result.failures),
+                'total_execution_time': result.timeTaken,
+            }
+            with open(self.report_path, 'w') as outfile:
+                json.dump(data, outfile)
+        else:
+            if test_result_count:
+                if SLOW_TEST_THRESHOLD_MS:
+                    print("\n{r} slowest tests over {ms}ms:".format(
+                        r=test_result_count, ms=SLOW_TEST_THRESHOLD_MS)
+                    )
+
+            if report:
+                for func_name, timing in test_results:
+                    if timing > valid_time:
+                        print("\n\033[31m{t:.4f}s\033[0m \033[94m{f}\033[0m".format(f=func_name, t=float(timing)))
+                    else:
+                        print("\n\033[32m{t:.4f}s\033[0m \033[94m{f}\033[0m".format(f=func_name, t=float(timing)))
+
+            if not len(test_results) and SLOW_TEST_THRESHOLD_MS:
+                print("\nNo tests slower than {ms}ms".format(
+                    ms=SLOW_TEST_THRESHOLD_MS)
+                )
+
+    def read_timing_files(self):
+        file_prefix = getattr(
+            settings, 'TESTS_REPORT_TMP_FILES_PREFIX', '_tests_report_'
+        )
+        files = glob.glob("{}*.txt".format(file_prefix))
+        for report_file in files:
+            yield report_file
+
+    def get_timings(self):
+        timings = []
+        for report_file in self.read_timing_files():
+            with open(report_file, "r") as f:
+                for line in f:
+                    name, duration = line.strip('\n').split(',')
+                    timings.append((name, float(duration)))
+            os.remove(report_file)
+        return timings
+
+    def remove_timing_tmp_files(self):
+        for report_file in self.read_timing_files():
+            os.remove(report_file)
+
+    def suite_result(self, suite, result):
+        return_value = super(DiscoverSlowestTestsRunner, self).suite_result(
+            suite, result
+        )
+
+        should_generate_report = (
+                getattr(settings, 'ALWAYS_GENERATE_SLOW_REPORT', True) or
+                self.should_generate_report
+        )
+        if not should_generate_report:
+            self.remove_timing_tmp_files()
+            return return_value
+
+        # Grab slowest tests
+        timings = self.get_timings()
+        by_time = sorted(timings, key=lambda x: x[1], reverse=True)
+
+        if by_time is not None and num_slow_tests:
+            by_time = by_time[:num_slow_tests]
+
+        test_results = by_time
+
+        if slow_test_threshold_ms:
+            # Filter tests by threshold
+            test_results = []
+
+            for result in by_time:
+                # Convert test time from seconds to miliseconds for comparison
+                result_time_ms = result[1] * 1000
+
+                # If the test was under the threshold
+                # don't show it to the user
+                if result_time_ms < slow_test_threshold_ms:
+                    continue
+
+                test_results.append(result)
+
+        self.generate_report(test_results, result)
+        return return_value
